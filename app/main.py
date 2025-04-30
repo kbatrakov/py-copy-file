d1376f2f07d7b5701736841caf4499ba23af366f
kbatrakov <kirillbatrakov0819@gmail.com>
2025-04-30 23:14:44 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if len(command.split()) >= 3:
+        try:
+            with (open(command.split()[1].lower(), "r") as initial_file,
+                  open(command.split()[2].lower(), "w+") as new_file):
+                if initial_file.name == new_file.name:
+                    return
+                else:
+                    new_file.write(initial_file.read())
+        except FileNotFoundError:
+            pass
